ce6ec96a503b4f308202d8af01322250220a5670
Cristian Czika <cristianczika6@gmail.com>
2026-07-23 13:22:41 +0300

    feat: refactor xml configuration to java-based configuration

--- spring-app/src/main/java/com/example/Main.java
+++ spring-app/src/main/java/com/example/Main.java
@@ -1,11 +1,11 @@
 package com.example;
 
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld helloWorld = (HelloWorld) context.getBean("helloBean");
         helloWorld.message();
     }
