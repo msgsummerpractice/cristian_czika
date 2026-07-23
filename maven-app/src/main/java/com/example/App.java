95fc9c5830a32590937e5d90d3ce27f4aa72a7f3
Cristian Czika <cristianczika6@gmail.com>
2026-07-23 14:21:12 +0300

    chore: fix formatting

--- maven-app/src/main/java/com/example/App.java
+++ maven-app/src/main/java/com/example/App.java
@@ -4,10 +4,9 @@ package com.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Environment: " + System.getProperty("env") );
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Environment: " + System.getProperty("env"));
     }
 }
