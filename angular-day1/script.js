52461ece3ccfa367ab7232c67b2af039956cc744
Cristian Czika <cristianczika6@gmail.com>
2026-07-29 11:37:10 +0300

    feat: replace js with ts

--- angular-day1/script.js
+++ angular-day1/script.js
@@ -1,19 +1,23 @@
+"use strict";
 let loading = false;
-
 async function getDogImage() {
-  const dogImage = document.getElementById('dogImage');
-  loading = true;
-  dogImage.hidden = false;
-
-  try {
-    dogImage.alt = "Loading...";
-    const response = await fetch('https://dog.ceo/api/breeds/image/random');
-    const data = await response.json();
-    dogImage.src = data.message;
-    loading = false;
-    dogImage.alt = "Random Dog Image";
-  } catch (error) {
-    console.error('Failed to fetch dog image:', error);
-    loading = false;
-  }
+    const dogImage = document.getElementById('dogImage');
+    if (!dogImage) {
+        console.error('Dog image element not found');
+        return;
+    }
+    loading = true;
+    dogImage.hidden = false;
+    try {
+        dogImage.alt = "Loading...";
+        const response = await fetch('https://dog.ceo/api/breeds/image/random');
+        const data = await response.json();
+        dogImage.src = data.message;
+        loading = false;
+        dogImage.alt = "Random Dog Image";
+    }
+    catch (error) {
+        console.error('Error fetching dog image:', error);
+        loading = false;
+    }
 }
