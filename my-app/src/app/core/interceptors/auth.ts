6b5af8fae0f10514a686838d77c92dd4646b5535
Cristian Czika <cristianczika6@gmail.com>
2026-08-04 16:28:19 +0300

    fix: authInterceptor

--- my-app/src/app/core/interceptors/auth.ts
+++ my-app/src/app/core/interceptors/auth.ts
@@ -6,6 +6,14 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const authService = inject(AuthService);
   const token = authService.getToken();
 
+  const bypassUrls = ['/api/v1/auth/signin', '/api/v1/auth/signup'];
+
+  const shouldBypass = bypassUrls.some((url) => req.url.includes(url));
+
+  if (shouldBypass) {
+    return next(req);
+  }
+
   const cloned = req.clone({
     setHeaders: {
       Authorization: `Bearer ${token}`,
